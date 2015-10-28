37d409791b245216a14c195251661318b31da274
RHODEN <RHODEN@univel.br>
2015-10-28 02:01:21 -0200

    Criada a Enum dos Generos

---
+++ src/main/java/br/univel/Genero.java
@@ -0,0 +1,17 @@
+package br.univel;
+
+public enum Genero {
+	
+	M("Masculino"),
+	F("Feminino");
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	private Genero(String nome) {
+		this.nome = nome;
+	}
+}
