e025014bccdff0672d867464e64ab779076d3958
RHODEN <RHODEN@univel.br>
2015-10-28 01:56:06 -0200

    Criada a Enum dos estados

---
+++ src/main/java/br/univel/Estado.java
@@ -0,0 +1,46 @@
+package br.univel;
+
+public enum Estado {
+	
+	AC("Acre"),
+	AL("Alagoas"),
+	AP("Amapá"),
+	AM("Amazonas"),
+	BA("Bahia"),
+	CE("Ceará"),
+	DF("Distrito Federal"),
+	ES("Espirito Santo"),
+	GO("Goiás"),
+	MA("Maranhão"),
+	MT("Mato Grosso"),
+	MS("Mato Grosso do Sul"),
+	MG("Minas Gerais"),
+	PA("Pará"),
+	PB("Paraíba"),
+	PR("Paraná"),
+	PE("Pernambuco"),
+	PI("Piauí"),
+	RJ("Rio de Janeiro"),
+	RN("Rio Grande do Norte"),
+	RS("Rio Grande do Sul"),
+	RO("Rondônia"),
+	RR("Roraima"),
+	SC("Santa Catarina"),
+	SP("São Paulo"),
+	SE("Sergipe"),
+	TO("Tocantins");
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+	
+	private Estado(String nome){
+		this.nome=nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
