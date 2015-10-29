3e8d49e99b1443bcb0aeb321ddca29e225de2207
RHODEN <RHODEN@univel.br>
2015-10-29 02:31:13 -0200

    Eclipse não está reconhecendo o método main =(

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TrabalhoBimestral
+29/10/2015
+* Tela de produtos, configuração em abas
--- src/main/java/br/univel/TelaPrincipal.java
+++ src/main/java/br/univel/TelaPrincipal.java
@@ -2,16 +2,17 @@ package br.univel;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class TelaPrincipal extends JFrame {
 
@@ -41,53 +42,54 @@ public class TelaPrincipal extends JFrame {
 		setTitle("Tela Principal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnCadastro = new JMenu("Cadastros");
 		menuBar.add(mnCadastro);
-		
+
 		JMenuItem mntmProdutos = new JMenuItem("Produtos");
 		mntmProdutos.addActionListener(new ActionListener() {
+			TelaProdutos tp = new TelaProdutos();
+
 			public void actionPerformed(ActionEvent arg0) {
-				
-				TelaProdutos tp = new TelaProdutos();
+
 				tabbedPane.addTab("Tela ", tp);
-				
+
 			}
 		});
 		mnCadastro.add(mntmProdutos);
-		
+
 		JMenuItem mntmClientes = new JMenuItem("Clientes");
 		mnCadastro.add(mntmClientes);
-		
+
 		JMenuItem mntmUsurios = new JMenuItem("Usu\u00E1rios");
 		mnCadastro.add(mntmUsurios);
-		
+
 		JMenu mnVendas = new JMenu("Vendas");
 		menuBar.add(mnVendas);
-		
+
 		JMenuItem mntmNovaVenda = new JMenuItem("Nova Venda");
 		mnVendas.add(mntmNovaVenda);
-		
+
 		JMenu mnRelatrios = new JMenu("Relat\u00F3rios");
 		menuBar.add(mnRelatrios);
-		
+
 		JMenuItem mntmNewMenuItem = new JMenuItem("Produtos");
 		mnRelatrios.add(mntmNewMenuItem);
-		
+
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Clientes");
 		mnRelatrios.add(mntmNewMenuItem_1);
-		
+
 		JMenuItem mntmVendas = new JMenuItem("Vendas");
 		mnRelatrios.add(mntmVendas);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+
+		JTabbedPane tabbedPane = new JTabbedPane(SwingConstants.TOP);
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 	}
 
