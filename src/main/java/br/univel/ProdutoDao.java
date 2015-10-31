2bd542d7a4f979e19eb002ed1c81e718dfa4817b
RHODEN <RHODEN@univel.br>
2015-10-31 17:01:56 -0200

    Commitando novamente pois havia esquecido de selecionar a interface antes de commitar...

---
+++ src/main/java/br/univel/ProdutoDao.java
@@ -0,0 +1,21 @@
+package br.univel;
+
+import java.util.List;
+/**
+ * @author user
+ * Classe criada para implementacao dos codigos sql dos produtos 
+ * 31/10/2015 às 16:51
+ */
+public interface ProdutoDao {
+
+	public void inserir(Produtos p);
+	
+	public void atualizar(Produtos p);
+	
+	public void excluir(Produtos p);
+	
+	public Produtos buscar(int id);
+	
+	public List<Produtos> listarProdutos();
+	
+}
