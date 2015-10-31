70ff42d56289962df235f12c5fd9b629fac79693
RHODEN <RHODEN@univel.br>
2015-10-31 17:07:08 -0200

    Criada a classe que fará as alteraçoes dos produtos

---
+++ src/main/java/br/univel/ProdutoDaoImplementacao.java
@@ -0,0 +1,41 @@
+package br.univel;
+
+import java.util.List;
+
+/**
+ * @author user
+ *
+ */
+public class ProdutoDaoImplementacao implements ProdutoDao {
+
+
+	@Override
+	public void inserir(Produtos p) {
+
+
+	}
+
+	@Override
+	public void atualizar(Produtos p) {
+
+
+	}
+
+	@Override
+	public void excluir(Produtos p) {
+
+
+	}
+	@Override
+	public Produtos buscar(int id) {
+
+		return null;
+	}
+
+	@Override
+	public List<Produtos> listarProdutos() {
+
+		return null;
+	}
+
+}
