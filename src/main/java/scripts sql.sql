91e7c2b058379e54efdb1624b32b019af37ceeb6
RHODEN <RHODEN@univel.br>
2015-11-03 13:31:37 -0200

    adicionado o script de criação do banco e das tabelas.

---
+++ src/main/java/scripts sql.sql
@@ -0,0 +1,40 @@
+﻿CREATE DATABASE "SistemaVendas"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
+CREATE TABLE clientes (
+			id int PRIMARY KEY NOT NULL,
+			nome varchar(40),
+			telefone varchar(40),
+			endereco varchar(40),
+			cidade varchar(40),
+			estado varchar(40),
+			genero varchar(10),
+			email varchar(40)
+			);
+CREATE TABLE usuarios (
+			id int PRIMARY KEY NOT NULL,
+			senha varchar(4),
+			id_cliente int NOT NULL references clientes(id)
+			);
+CREATE TABLE produtos (
+			id int PRIMARY KEY NOT NULL,
+			descricao varchar(40),
+			departamento varchar(40),
+			unidade varchar(8),
+			custo smallint,
+			margem smallint			
+			);
+CREATE TABLE vendas (
+			id int PRIMARY KEY NOT NULL,
+			id_cliente int NOT NULL references clientes(id),
+			id_produto int NOT NULL references produtos(id),
+			quantidade int,
+			total float,
+			totalpago float,
+			troco float
+			);
