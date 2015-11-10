ec4ed4121845d3364077164ebc79236028b0b647
univel <univel@univel.br>
2015-11-10 20:31:09 -0200

    Erros no Maven, Erros no Pom.xml. Perdi muito tempo com erros... Agora implementando o Produto Dao

--- src/main/java/br/univel/banco/ProdutoDaoImplementacao.java
+++ src/main/java/br/univel/banco/ProdutoDaoImplementacao.java
@@ -1,27 +1,81 @@
 package br.univel.banco;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import br.univel.classes.Produtos;
 
 /**
- * @author user
- *
+ * @author Diego Alberto Rhoden
  */
 public abstract class ProdutoDaoImplementacao implements DaoGenerico<Produtos> {
 
-	@Override
-	public void inserir(Produtos p) {
+			
+		private PreparedStatement ps = null;
+		private Statement st = null;
+		private ResultSet rs = null;
+		private Produtos p = null;
+		private List<Produtos> lista = null;
+		private Connection con =  Conexao.getInstance().conOpen();
+		
+		@Override
+		public void inserir(Produtos pd) {
+			try {
+				ps = con
+						.prepareStatement("INSERT INTO produtos (barras, categoria, descricao, unidade, custo, margem) VALUES (?, ?, ?, ?, ?, ?)");
+				ps.setInt(1, pd.getBarras());
+				ps.setString(2, pd.getCategoria());
+				ps.setString(3, pd.getDescricao());
+				ps.setString(4, pd.getUnidade());
+				ps.setBigDecimal(5, pd.getCusto());
+				ps.setBigDecimal(6, pd.getMargem());
+				ps.executeUpdate();
+				ps.close();
+				JOptionPane.showMessageDialog(null, "Produtos: " + pd.getDescricao()
+						+ "\n Cadastrado com sucesso.");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+	
 
+	@Override
+	public void atualizar(Produtos pd) {
+		try {
+			ps = con
+					.prepareStatement("UPDATE produtos SET barras = ?, categoria = ?, descricao = ?,"
+							+ " unidade = ?, custo = ?, margem = ? WHERE id = " + pd.getId());
+			ps.setInt(1, pd.getBarras());
+			ps.setString(2, pd.getCategoria());
+			ps.setString(3, pd.getDescricao());
+			ps.setString(4, pd.getUnidade());
+			ps.setBigDecimal(5, pd.getCusto());
+			ps.setBigDecimal(6, pd.getMargem());
+			ps.executeUpdate();
+			ps.close();
+			JOptionPane.showMessageDialog(null, "Produtos: " + pd.getDescricao()
+					+ "\n Atualizado com sucesso.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Override
-	public void atualizar(Produtos p) {
-
-	}
-
-	public void excluir(Produtos p) {
-
+	public void excluir(Produtos pd) {
+		try {
+			ps =con.prepareStatement("DELETE FROM produtos WHERE id =" + pd.getId());
+			int res = ps.executeUpdate();
+			ps.close();
+			JOptionPane.showMessageDialog(null, "Produtos excluido com sucesso.");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	// public Produtos buscar(int id) {
